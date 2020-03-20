b11c05aec252b0e52eec78899b16de72fa03a797
Fabien Joseph <styleure@ascentia.fr>
2020-03-20 15:00:48 +0100

    Creation of modules mission-api and mission-business

---
+++ lazybot-microservices/lazybot-mission/lazybot-mission-api/src/main/java/com/lazybot/microservices/mission/api/lazybotmissionapi/LazybotMissionApiApplication.java
@@ -0,0 +1,13 @@
+package com.lazybot.microservices.mission.api.lazybotmissionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazybotMissionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazybotMissionApiApplication.class, args);
+	}
+
+}
---
+++ lazybot-microservices/lazybot-mission/lazybot-mission-api/src/test/java/com/lazybot/microservices/mission/api/lazybotmissionapi/LazybotMissionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazybot.microservices.mission.api.lazybotmissionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazybotMissionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
