f10564fe7abe33d8014111e8d7978da82bbdf00e
Fabien Joseph <styleure@ascentia.fr>
2020-03-21 09:36:53 +0100

    Bot model has now a method to start() and stop() himself

--- lazybot-microservices/lazybot-mission/lazybot-mission-api/src/main/java/com/lazybot/microservices/mission/api/socket/MissionSocket.java
+++ lazybot-microservices/lazybot-mission/lazybot-mission-api/src/main/java/com/lazybot/microservices/mission/api/socket/MissionSocket.java
@@ -6,8 +6,6 @@ import com.corundumstudio.socketio.SocketIOServer;
 import com.lazybot.microservices.mission.model.Bot;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class MissionSocket {
 
@@ -16,14 +14,8 @@ public class MissionSocket {
     }
 
     public void connectBot(SocketIOClient client, Bot bot, AckRequest ackSender) throws Exception {
-        ProcessBuilder pb = new ProcessBuilder("node",
-                "C:\\Users\\Fabien\\IdeaProjects\\lazybot\\lazybot-robot\\robot.js",
-                "--username=" + bot.getUsername(),
-                "--password=" + bot.getPassword());
-        pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-        pb.redirectError(ProcessBuilder.Redirect.INHERIT);
-        Process p = pb.start();
+        bot.start();
         Thread.sleep(5000);
-        p.destroy();
+        bot.stop();
     }
 }
--- lazybot-microservices/lazybot-mission/lazybot-mission-model/src/main/java/com/lazybot/microservices/mission/model/Bot.java
+++ lazybot-microservices/lazybot-mission/lazybot-mission-model/src/main/java/com/lazybot/microservices/mission/model/Bot.java
@@ -4,12 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.io.IOException;
+
 @Getter
 @Setter
 @ToString
 public class Bot {
     String username;
     String password;
+    Process process;
 
     public Bot() {
     }
@@ -17,5 +20,19 @@ public class Bot {
     public Bot(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+
+    public void start() throws IOException {
+        ProcessBuilder pb = new ProcessBuilder("node",
+                "C:\\Users\\Fabien\\IdeaProjects\\lazybot\\lazybot-robot\\robot.js",
+                "--username=" + this.getUsername(),
+                "--password=" + this.getPassword());
+        pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        pb.redirectError(ProcessBuilder.Redirect.INHERIT);
+        this.process = pb.start();
+    }
+
+    public void stop () {
+        this.process.destroy();
     }
 }
