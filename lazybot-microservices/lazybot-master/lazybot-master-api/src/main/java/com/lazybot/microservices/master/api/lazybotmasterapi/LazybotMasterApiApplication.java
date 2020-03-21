20eb300c38b4288621116d05749d670091b3d5e2
Fabien Joseph <styleure@ascentia.fr>
2020-03-21 09:53:07 +0100

    Create lazybot-master-api module

---
+++ lazybot-microservices/lazybot-master/lazybot-master-api/src/main/java/com/lazybot/microservices/master/api/lazybotmasterapi/LazybotMasterApiApplication.java
@@ -0,0 +1,13 @@
+package com.lazybot.microservices.master.api.lazybotmasterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazybotMasterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazybotMasterApiApplication.class, args);
+	}
+
+}
---
+++ lazybot-microservices/lazybot-master/lazybot-master-api/src/test/java/com/lazybot/microservices/master/api/lazybotmasterapi/LazybotMasterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazybot.microservices.master.api.lazybotmasterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazybotMasterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
