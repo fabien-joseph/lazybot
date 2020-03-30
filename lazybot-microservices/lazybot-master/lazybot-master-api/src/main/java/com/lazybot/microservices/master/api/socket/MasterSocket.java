7537c7c556f5c33f8823e9c810e40fe45b7ec63e
Fabien Joseph <styleure@ascentia.fr>
2020-03-30 17:17:46 +0200

    push before rework infrastructure (server/client)

--- lazybot-microservices/lazybot-master/lazybot-master-api/src/main/java/com/lazybot/microservices/master/api/socket/MasterSocket.java
+++ lazybot-microservices/lazybot-master/lazybot-master-api/src/main/java/com/lazybot/microservices/master/api/socket/MasterSocket.java
@@ -3,21 +3,46 @@ package com.lazybot.microservices.master.api.socket;
 import com.corundumstudio.socketio.AckRequest;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import org.springframework.stereotype.Service;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class MasterSocket {
     private Socket clientMission;
     private Socket clientMap;
+    private Map<String, SocketIOClient> bots;
     public MasterSocket(SocketIOServer server) throws URISyntaxException {
+        bots = new HashMap<>();
         this.clientMission = IO.socket("http://localhost:9091");
         this.clientMap = IO.socket("http://localhost:9092");
         this.clientMission.connect();
         this.clientMap.connect();
+
+        server.addEventListener("disconnectBot", String.class, this::disconnectBot);
+        server.addEventListener("connectBot", String.class, this::connectBot);
+        server.addEventListener("test", String.class, this::testPassClient);
         server.addEventListener("getChunk", Integer.class, this::getChunk);
+    }
+
+    private void connectBot(SocketIOClient socketIOClient, String id, AckRequest ackRequest) throws InterruptedException {
+        System.out.println("CONNEXION de " + id );
+        bots.put(id, socketIOClient);
+    }
+    private void disconnectBot(SocketIOClient socketIOClient, String id, AckRequest ackRequest) {
+        System.out.println("Déconnexion de " + id);
+        bots.remove(id.toString());
+    }
+
+    private void testPassClient(SocketIOClient socketIOClient, String test, AckRequest ackRequest)
+    {
+        System.out.println("TEST DE PASSAGE");
+        //clientMap.emit("callClient", socketIOClient);
     }
 
     public void getChunk(SocketIOClient client, Integer ray, AckRequest ackSender) throws Exception {
--- lazybot-robot/robot.js
+++ lazybot-robot/robot.js
@@ -1,20 +1,35 @@
 const io = require("socket.io-client"),
-    ioClient = io.connect("http://localhost:9092");
+    ioMaster = io.connect("http://localhost:9090"),
+    //ioMission = io.connect("http://localhost:9091"),
+    ioMap = io.connect("http://localhost:9092");
 
+var botId;
 var mybot = require('./business/mybot');
 var mineflayer = require('mineflayer');
 var bot = mybot.connect(process.argv, process.env.BOT_USERNAME, process.env.BOT_PASSWORD);
+
+bot.on('login', function () {
+    botId = Math.floor(Math.random() * (999999 - 100000) + 100000);
+    ioMap.emit('connectBot', botId);
+    ioMaster.emit('connectBot', botId);
+});
+
+bot.on('end', function () {
+    ioMaster.emit('disconnect', botId);
+});
 
 bot.on('chat', function(username, message) {
     if (username === bot.username) return;
     if (message === 'chunk') {
         let blocks = mybot.loadChunkArround(bot, 10, 0, 0);
-        //ioClient.emit('loadChunk', mybot.loadChunkArround(bot, 1));
+        //ioMaster.emit('loadChunk', mybot.loadChunkArround(bot, 1));
         return;
     }
     bot.chat(message);
 });
 
-ioClient.on('loadChunk', function (ray, offsetX, offsetZ) {
 
+ioMap.on('test', function (ray, offsetX, offsetZ) {
+    console.log("Test reçu")
+    bot.chat("Recu");
 });
