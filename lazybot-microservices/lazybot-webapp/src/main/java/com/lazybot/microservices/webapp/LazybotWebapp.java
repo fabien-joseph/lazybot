ece67531f8359336653c8fddd0d1af6dfe795adf
Fabien Joseph <styleure@ascentia.fr>
2020-06-26 11:14:52 +0200

    Correct a problem with a call of a wrong lib

--- lazybot-microservices/lazybot-webapp/src/main/java/com/lazybot/microservices/webapp/LazybotWebapp.java
+++ lazybot-microservices/lazybot-webapp/src/main/java/com/lazybot/microservices/webapp/LazybotWebapp.java
@@ -8,7 +8,6 @@ import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Hello world!
